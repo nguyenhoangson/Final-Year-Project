a1ade0702c2d43647a064c58b8bf6344953329e0
Nguyen Hoang Son <nguyenhoangson@example.com>
2016-09-13 23:55:52 +0800

    Set up folders

---
+++ tensorflow/MNIST.py
@@ -0,0 +1,3 @@
+### Code for training simple feed-forward network
+### neural network for handwritten digit classification
+### using the (classic) MNIST data set 
---
+++ tensorflow/play_tf.py
@@ -0,0 +1,10 @@
+### Play and experiment with TensorFlow
+
+import tensorflow as tf
+
+sess = tf.Session()
+
+a = tf.constant(10)
+b = tf.constant(15)
+
+print(sess.run(a+b))
